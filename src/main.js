91e21854c24a8ecae35bf1d66ec08bfabd78cad7
mehran mohammadi <mohammadi000098@gmail.com>
2024-03-27 18:50:59 +0330

    fix

--- src/main.js
+++ src/main.js
@@ -1,37 +1,38 @@
 import Splide from "@splidejs/splide";
 import "./index.css";
 
-var productCounter = {
-  count: 0,
-  incrementCounter: function () {
-    if (this.count < 10) {
-      return (this.count = this.count + 1);
-    } else {
-      return this.count;
-    }
-  },
-  decrementCounter: function () {
-    if (this.count > 0) {
-      return (this.count = this.count - 1);
-    } else {
-      return (this.count = 0);
-    }
-  },
-  resetCounter: function () {
-    return (this.count = 0);
-  },
-};
-
-var displayCout = document.getElementById("displayCounter");
-displayCout.innerHTML = 0;
-document.getElementById("increment").onclick = function () {
-  displayCout.innerHTML = productCounter.incrementCounter();
-};
-document.getElementById("decrement").onclick = function () {
-  displayCout.innerHTML = productCounter.decrementCounter();
-};
-
 document.addEventListener("DOMContentLoaded", function () {
+  var productCounter = {
+    count: 0,
+    incrementCounter: function () {
+      if (this.count < 10) {
+        return (this.count = this.count + 1);
+      } else {
+        return this.count;
+      }
+    },
+    decrementCounter: function () {
+      if (this.count > 0) {
+        return (this.count = this.count - 1);
+      } else {
+        return (this.count = 0);
+      }
+    },
+    resetCounter: function () {
+      return (this.count = 0);
+    },
+  };
+
+  var displayCout = document.getElementById("displayCounter");
+  displayCout.innerHTML = 0;
+
+  document.getElementById("increment").onclick = function () {
+    displayCout.innerHTML = productCounter.incrementCounter();
+  };
+  document.getElementById("decrement").onclick = function () {
+    displayCout.innerHTML = productCounter.decrementCounter();
+  };
+
   var splideCheck = document.getElementsByClassName("splide");
   if (splideCheck.length) {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
