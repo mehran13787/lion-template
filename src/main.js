1ee3d88e85fb51f6e4358dd52e58f94fa8f45077
mehran mohammadi <mohammadi000098@gmail.com>
2024-05-13 20:40:02 +0330

    fix : clean js files

--- src/main.js
+++ src/main.js
@@ -1,111 +1,36 @@
-import Splide from "@splidejs/splide";
 import "./index.css";
 
 document.addEventListener("DOMContentLoaded", function () {
-  var productCounter = {
-    count: 0,
-    incrementCounter: function () {
-      if (this.count < 10) {
-        return (this.count = this.count + 1);
-      } else {
-        return this.count;
-      }
-    },
-    decrementCounter: function () {
-      if (this.count > 0) {
-        return (this.count = this.count - 1);
-      } else {
-        return (this.count = 0);
-      }
-    },
-    resetCounter: function () {
-      return (this.count = 0);
-    },
-  };
-
-  var displayCout = document.getElementById("displayCounter");
-  displayCout.innerHTML = 0;
-
-  document.getElementById("increment").onclick = function () {
-    displayCout.innerHTML = productCounter.incrementCounter();
-  };
-  document.getElementById("decrement").onclick = function () {
-    displayCout.innerHTML = productCounter.decrementCounter();
-  };
-
-  var splideCheck = document.getElementsByClassName("splide");
-  if (splideCheck.length) {
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // first-carousel
-    var splide1 = new Splide("#first-carousel-splide", {
-      type: "loop",
-      perPage: 6,
-      arrows: false,
-      pagination: false,
-      lazyLoad: "nearby",
-      breakpoints: {
-        1382: {
-          perPage: 5,
-        },
-        900: {
-          perPage: 4,
-        },
-        570: {
-          perPage: 3,
-        },
-        470: {
-          perPage: 2,
-          padding: "4rem",
-        },
-        450: {
-          perPage: 2,
-          padding: "2rem",
-        },
-        360: {
-          perPage: 2,
-          padding: "0rem",
-        },
+  if (document.getElementById("product-detail")) {
+    var productCounter = {
+      count: 0,
+      incrementCounter: function () {
+        if (this.count < 10) {
+          return (this.count = this.count + 1);
+        } else {
+          return this.count;
+        }
       },
-    });
+      decrementCounter: function () {
+        if (this.count > 0) {
+          return (this.count = this.count - 1);
+        } else {
+          return (this.count = 0);
+        }
+      },
+      resetCounter: function () {
+        return (this.count = 0);
+      },
+    };
 
-    document
-      .querySelector(".first-carousel-next")
-      .addEventListener("click", (e) => {
-        splide1.go("+1");
-      });
+    var displayCout = document.getElementById("displayCounter");
+    displayCout.innerHTML = 0;
 
-    document
-      .querySelector(".first-carousel-prev")
-      .addEventListener("click", (e) => {
-        splide1.go("-1");
-      });
-
-    splide1.mount();
+    document.getElementById("increment").onclick = function () {
+      displayCout.innerHTML = productCounter.incrementCounter();
+    };
+    document.getElementById("decrement").onclick = function () {
+      displayCout.innerHTML = productCounter.decrementCounter();
+    };
   }
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  var main = new Splide("#main-carousel", {
-    type: "fade",
-    rewind: true,
-    pagination: true,
-    arrows: false,
-  });
-  var tumbnail = new Splide("#thumbnail-carousel", {
-    fixedWidth: 90,
-    gap: 10,
-    rewind: true,
-    pagination: false,
-    isNavigation: true,
-    arrows: false,
-  });
-
-  tumbnail.mount();
-  main.mount();
-  main.sync(tumbnail);
-  //////////////////////////////////////////
-
-  // let button;
-  // button = document.getElementsByClassName(".header-category");
-  // button.addEventListener("click", () => {
-  //   console.log("x");
-  // });
 });
--- src/slider.js
+++ src/slider.js
@@ -5,185 +5,253 @@ document.addEventListener("DOMContentLoaded", function () {
   if (splideCheck.length) {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // zero-carousel
-    var splide0 = new Splide("#zero-carousel-splide", {
-      type: "loop",
-      perPage: 1,
-      arrows: false,
-      autoplay: true,
-      lazyLoad: "nearby",
-    });
-
-    splide0.mount();
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // first-carousel
-    var splide1 = new Splide("#first-carousel-splide", {
-      type: "loop",
-      perPage: 6,
-      arrows: false,
-      pagination: false,
-      lazyLoad: "nearby",
-      breakpoints: {
-        1382: {
-          perPage: 5,
-        },
-        900: {
-          perPage: 4,
-        },
-        570: {
-          perPage: 3,
-        },
-        470: {
-          perPage: 2,
-          padding: "4rem",
-        },
-        450: {
-          perPage: 2,
-          padding: "2rem",
-        },
-        360: {
-          perPage: 2,
-          padding: "0rem",
-        },
-      },
-    });
-
-    document
-      .querySelector(".first-carousel-next")
-      .addEventListener("click", (e) => {
-        splide1.go("+1");
+    if (document.getElementById("main-page")) {
+      var splide0 = new Splide("#zero-carousel-splide", {
+        type: "loop",
+        perPage: 1,
+        arrows: false,
+        autoplay: true,
+        lazyLoad: "nearby",
       });
 
-    document
-      .querySelector(".first-carousel-prev")
-      .addEventListener("click", (e) => {
-        splide1.go("-1");
+      splide0.mount();
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      // first-carousel
+      var splide1 = new Splide("#first-carousel-splide", {
+        type: "loop",
+        perPage: 6,
+        arrows: false,
+        pagination: false,
+        lazyLoad: "nearby",
+        breakpoints: {
+          1382: {
+            perPage: 5,
+          },
+          900: {
+            perPage: 4,
+          },
+          570: {
+            perPage: 3,
+          },
+          470: {
+            perPage: 2,
+            padding: "4rem",
+          },
+          450: {
+            perPage: 2,
+            padding: "2rem",
+          },
+          360: {
+            perPage: 2,
+            padding: "0rem",
+          },
+        },
       });
 
-    splide1.mount();
+      document
+        .querySelector(".first-carousel-next")
+        .addEventListener("click", (e) => {
+          splide1.go("+1");
+        });
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // second-carousel
-    var splide2 = new Splide("#second-carousel-splide", {
-      type: "loop",
-      perPage: 4,
-      arrows: false,
-      pagination: false,
-      lazyLoad: "nearby",
-      breakpoints: {
-        1400: {
-          perPage: 3,
-          padding: "2rem",
-        },
-        1100: {
-          perPage: 2,
-          padding: "2rem",
-        },
-        569: {
-          perPage: 2,
-          padding: "0rem",
-        },
-        410: {
-          perPage: 1,
-          padding: "5rem",
-        },
-        390: {
-          perPage: 1,
-          padding: "3.5rem",
-        },
-      },
-    });
+      document
+        .querySelector(".first-carousel-prev")
+        .addEventListener("click", (e) => {
+          splide1.go("-1");
+        });
 
-    document
-      .querySelector(".second-carousel-next")
-      .addEventListener("click", (e) => {
-        splide2.go("+1");
+      splide1.mount();
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      // second-carousel
+      var splide2 = new Splide("#second-carousel-splide", {
+        type: "loop",
+        perPage: 4,
+        arrows: false,
+        pagination: false,
+        lazyLoad: "nearby",
+        breakpoints: {
+          1400: {
+            perPage: 3,
+            padding: "2rem",
+          },
+          1100: {
+            perPage: 2,
+            padding: "2rem",
+          },
+          569: {
+            perPage: 2,
+            padding: "0rem",
+          },
+          410: {
+            perPage: 1,
+            padding: "5rem",
+          },
+          390: {
+            perPage: 1,
+            padding: "3.5rem",
+          },
+        },
       });
 
-    document
-      .querySelector(".second-carousel-prev")
-      .addEventListener("click", (e) => {
-        splide2.go("-1");
+      document
+        .querySelector(".second-carousel-next")
+        .addEventListener("click", (e) => {
+          splide2.go("+1");
+        });
+
+      document
+        .querySelector(".second-carousel-prev")
+        .addEventListener("click", (e) => {
+          splide2.go("-1");
+        });
+
+      splide2.mount();
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      // third-carousel
+      var splide3 = new Splide("#third-carousel-splide", {
+        type: "loop",
+        perPage: 7,
+        arrows: false,
+        pagination: false,
+        lazyLoad: "nearby",
+        padding: "1rem",
+        breakpoints: {
+          900: {
+            perPage: 5,
+          },
+          700: {
+            perPage: 4,
+          },
+          570: {
+            perPage: 3,
+          },
+          470: {
+            perPage: 2,
+          },
+          360: {
+            perPage: 1,
+          },
+        },
       });
 
-    splide2.mount();
+      document
+        .querySelector(".third-carousel-next")
+        .addEventListener("click", (e) => {
+          splide3.go("+1");
+        });
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // third-carousel
-    var splide3 = new Splide("#third-carousel-splide", {
-      type: "loop",
-      perPage: 7,
-      arrows: false,
-      pagination: false,
-      lazyLoad: "nearby",
-      padding: "1rem",
-      breakpoints: {
-        900: {
-          perPage: 5,
-        },
-        700: {
-          perPage: 4,
-        },
-        570: {
-          perPage: 3,
-        },
-        470: {
-          perPage: 2,
-        },
-        360: {
-          perPage: 1,
-        },
-      },
-    });
+      document
+        .querySelector(".third-carousel-prev")
+        .addEventListener("click", (e) => {
+          splide3.go("-1");
+        });
 
-    document
-      .querySelector(".third-carousel-next")
-      .addEventListener("click", (e) => {
-        splide3.go("+1");
+      splide3.mount();
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      // fourth-carousel
+      var splide4 = new Splide("#fourth-carousel-splide", {
+        type: "loop",
+        perPage: 4,
+        arrows: false,
+        pagination: false,
+        lazyLoad: "nearby",
+        breakpoints: {
+          900: {
+            perPage: 3,
+          },
+          700: {
+            perPage: 2,
+          },
+          570: {
+            perPage: 1,
+          },
+        },
       });
 
-    document
-      .querySelector(".third-carousel-prev")
-      .addEventListener("click", (e) => {
-        splide3.go("-1");
-      });
+      document
+        .querySelector(".fourth-carousel-next")
+        .addEventListener("click", (e) => {
+          splide4.go("+1");
+        });
 
-    splide3.mount();
+      document
+        .querySelector(".fourth-carousel-prev")
+        .addEventListener("click", (e) => {
+          splide4.go("-1");
+        });
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // fourth-carousel
-    var splide4 = new Splide("#fourth-carousel-splide", {
-      type: "loop",
-      perPage: 4,
-      arrows: false,
-      pagination: false,
-      lazyLoad: "nearby",
-      breakpoints: {
-        900: {
-          perPage: 3,
-        },
-        700: {
-          perPage: 2,
-        },
-        570: {
-          perPage: 1,
-        },
-      },
-    });
+      splide4.mount();
+    }
 
-    document
-      .querySelector(".fourth-carousel-next")
-      .addEventListener("click", (e) => {
-        splide4.go("+1");
-      });
-
-    document
-      .querySelector(".fourth-carousel-prev")
-      .addEventListener("click", (e) => {
-        splide4.go("-1");
-      });
-
-    splide4.mount();
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    if (document.getElementById("product-detail")) {
+      var splide7 = new Splide("#seven-carousel-splide", {
+        type: "loop",
+        perPage: 6,
+        arrows: false,
+        pagination: false,
+        lazyLoad: "nearby",
+        breakpoints: {
+          1382: {
+            perPage: 5,
+          },
+          900: {
+            perPage: 4,
+          },
+          570: {
+            perPage: 3,
+          },
+          470: {
+            perPage: 2,
+            padding: "4rem",
+          },
+          450: {
+            perPage: 2,
+            padding: "2rem",
+          },
+          360: {
+            perPage: 2,
+            padding: "0rem",
+          },
+        },
+      });
+      document
+        .querySelector(".seven-carousel-next")
+        .addEventListener("click", (e) => {
+          splide7.go("+1");
+        });
+
+      document
+        .querySelector(".seven-carousel-prev")
+        .addEventListener("click", (e) => {
+          splide7.go("-1");
+        });
+
+      splide7.mount();
+      ///////////////////////////////////////////////////////////////////////////////
+
+      var main = new Splide("#main-carousel", {
+        type: "fade",
+        rewind: true,
+        pagination: true,
+        arrows: false,
+      });
+      var tumbnail = new Splide("#thumbnail-carousel", {
+        fixedWidth: 90,
+        gap: 10,
+        rewind: true,
+        pagination: false,
+        isNavigation: true,
+        arrows: false,
+      });
+
+      tumbnail.mount();
+      main.mount();
+      main.sync(tumbnail);
+    }
   }
 });
