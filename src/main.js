d85e5b04c3b182e39b200e1a8cf90a19f4bc3b19
mehran mohammadi <mohammadi000098@gmail.com>
2024-05-14 14:04:07 +0330

    chote : add category main

--- src/main.js
+++ src/main.js
@@ -33,4 +33,76 @@ document.addEventListener("DOMContentLoaded", function () {
       displayCout.innerHTML = productCounter.decrementCounter();
     };
   }
+  if (document.getElementById("main-page")) {
+    const categoryButton = document.getElementById("header-category");
+    const categorySection = document.getElementById("category-section");
+    categoryButton.addEventListener("click", () => {
+      if (categorySection.classList.contains("hidden")) {
+        categorySection.classList.replace("hidden", "block");
+      } else {
+        categorySection.classList.replace("block", "hidden");
+      }
+    });
+    categorySection.addEventListener("click", (e) => {
+      if (e.target.nodeName === "DIV") return;
+      if (categorySection.classList.contains("hidden")) {
+        categorySection.classList.replace("hidden", "block");
+      } else {
+        categorySection.classList.replace("block", "hidden");
+      }
+    });
+  }
+
+  const categoryCards = document.querySelectorAll(".category-card");
+
+  function reset() {
+    categoryCards.forEach((item) => {
+      const firstlayerText = item.querySelector(".layer-text");
+
+      firstlayerText.classList.replace("text-[#256CFA]", "text-[#0F1729]");
+      item.classList.replace("open", "close");
+      item.classList.remove(
+        "bg-gradient-to-r",
+        "from-[#FFFFFF1F]",
+        "to-[#FFFFFF]",
+        "border-[1px]",
+        "border-t-transparent",
+        "border-l-transparent",
+        "border-[#0000001F]"
+      );
+    });
+  }
+  categoryCards.forEach((item) => {
+    item.addEventListener("click", () => {
+      reset();
+
+      const firstlayerText = item.querySelector(".layer-text");
+
+      if (item.classList.contains("close")) {
+        firstlayerText.classList.replace("text-[#0F1729]", "text-[#256CFA]");
+        item.classList.replace("close", "open");
+        item.classList.add(
+          "bg-gradient-to-r",
+          "from-[#FFFFFF1F]",
+          "to-[#FFFFFF]",
+          "border-[1px]",
+          "border-t-transparent",
+          "border-l-transparent",
+          "border-[#0000001F]"
+        );
+      } else {
+        firstlayerText.classList.replace("text-[#256CFA]", "text-[#0F1729]");
+        item.classList.replace("open", "close");
+        item.classList.remove(
+          "bg-gradient-to-r",
+          "from-[#FFFFFF1F]",
+          "to-[#FFFFFF]",
+          "border-[1px]",
+          "border-t-transparent",
+          "border-l-transparent",
+          "border-[#0000001F]"
+        );
+      }
+    });
+  });
 });
