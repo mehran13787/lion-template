b718aef89b28d7c54a4742f9e451bf3e4cfd4d66
mehran mohammadi <mohammadi000098@gmail.com>
2024-06-04 18:28:43 +0330

    feat : add 404 page

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
     "shopping-cart-second-step.html",
     "shopping-cart-third-step.html",
     "successful-payment.html",
+    "error.html",
     "./src/**/*.{js,jsx,ts,tsx,html}",
   ],
   theme: {
