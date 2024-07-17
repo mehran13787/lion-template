d9a77e9dd01d697e3aa4bded0c81a59bab1a3f33
mehran mohammadi <mohammadi000098@gmail.com>
2024-07-17 23:33:26 +0330

    fix tailwind problem

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from "path";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
+  base: "./",
   plugins: [
     createHtmlPlugin({
       minify: true,
