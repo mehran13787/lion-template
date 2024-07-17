0f3cafb32d8e1c5ac452cb00b09f5aa1fcd4f18d
mehran mohammadi <mohammadi000098@gmail.com>
2024-07-17 23:22:20 +0330

    fix path problem

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { createHtmlPlugin } from "vite-plugin-html";
+
+export default defineConfig({
+  plugins: [
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        articles: resolve(__dirname, "articles.html"),
+        articleDetail: resolve(__dirname, "article-detail.html"),
+        categoryMobile: resolve(__dirname, "category-mobile.html"),
+        error: resolve(__dirname, "error.html"),
+        productDetails: resolve(__dirname, "product-details.html"),
+        productsMobile: resolve(__dirname, "products-mobile.html"),
+        products: resolve(__dirname, "products.html"),
+        shoppingCart: resolve(__dirname, "shopping-cart.html"),
+        shoppingCartSecondStep: resolve(
+          __dirname,
+          "shopping-cart-second-step.html"
+        ),
+        shoppingCartThirdStep: resolve(
+          __dirname,
+          "shopping-cart-third-step.html"
+        ),
+        successfulPayment: resolve(__dirname, "successful-payment.html"),
+      },
+    },
+  },
+});
